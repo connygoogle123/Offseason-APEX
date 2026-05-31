1d36712c0b1dbf14f548c8573e84862af736ee46
connygoogle123 <hellospirit28@gmail.com>
2026-05-30 21:43:23 -0400

    added pidf for turret tuning

--- Teamcode/src/main/java/org/firstinspires/ftc/teamcode/config/subsystems/TurretTable.java
+++ Teamcode/src/main/java/org/firstinspires/ftc/teamcode/config/subsystems/TurretTable.java
@@ -1,24 +1,46 @@
 package org.firstinspires.ftc.teamcode.config.subsystems;
 
+import com.seattlesolvers.solverslib.util.InterpLUT;
+
 public class TurretTable {
+    private static final InterpLUT angleILUT = new InterpLUT();
+    private static final InterpLUT speedILUT = new InterpLUT();
 
-    public static class TurretTuning {
-
-        public double p;
-        public double velocity;
-        public double f;
-
-        public TurretTuning (double p, double f, double velocity) {
-            this.p = p;
-            this.f = f;
-            this.velocity = velocity;
-
-
-        }
+    public static double getSpeed(double dist) {
+        return speedILUT.get(dist);
     }
 
-public TurretTable (){
+    public static double getAngle(double dist) {
+        return angleILUT.get(dist);
+    }
+    static {
+        angleILUT.add(60,)
+        speedILUT.add(60,)
 
-}
+        angleILUT.add(60,)
+        speedILUT.add(60,)
 
+        angleILUT.add(60,)
+        speedILUT.add(60,)
+
+        angleILUT.add(60,)
+        speedILUT.add(60,)
+
+        angleILUT.add(60,)
+        speedILUT.add(60,)
+
+        angleILUT.add(60,)
+        speedILUT.add(60,)
+
+        angleILUT.add(60,)
+        speedILUT.add(60,)
+
+        angleILUT.add(60,)
+        speedILUT.add(60,)
+
+
+        speedILUT.createLUT();
+        angleILUT.createLUT();
+
+    }
 }
